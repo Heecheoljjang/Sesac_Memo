1d0429b6fe4a012e41a330efdac5918e4c43217b
Heecheoljjang <kkll135@naver.com>
2022-09-01 01:41:31 +0900

    BaseView, BaseViewController 생성

--- Sesac_Memo/Utility/BaseView.swift
+++ Sesac_Memo/Utility/BaseView.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import UIKit
+
+class BaseView: UIView {
+ 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+        setUpConstraints()
+    }
+    
+    //사용하지 않는 코드이므로
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {}
+    
+    func setUpConstraints() {}
+    
+}
--- Sesac_Memo/Utility/BaseViewController.swift
+++ Sesac_Memo/Utility/BaseViewController.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configure()
+        setUpController()
+    }
+    
+    func configure() {}
+    
+    func setUpController() {}
+}
